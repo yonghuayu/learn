b740a3455d50867ed2bfba6986f371899fd50474
yuyonghua <yuyonghua2016@163.com>
2019-07-08 16:35:53 +0800

    ldap,webflux

---
+++ webflux/src/main/java/com/yu/learn/webflux/WebfluxApplication.java
@@ -0,0 +1,18 @@
+package com.yu.learn.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * webflux
+ *
+ * @author yuyonghua
+ * @date 2019/7/8 16:32
+ */
+@SpringBootApplication
+public class WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxApplication.class, args);
+    }
+}
